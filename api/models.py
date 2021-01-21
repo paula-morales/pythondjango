8b74d1d5db1525122a019f52f7f1d03dbb9b120b
paula-morales <pmpoclava@gmail.com>
2021-01-21 19:06:39 +0100

    model updated

--- api/models.py
+++ api/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Card(models.Model):
-    definition = models.CharField(max_length=200)
-    concept = models.CharField(max_length=20)
-    topic = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    imgUrl = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.definition
+        return self.title
--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview"),
-    path('card-list/', views.cardList, name="card-list"),
-    path('card-detail/<str:pk>/', views.cardDetail, name="card-Detail"),
-    path('card-update/<str:pk>/', views.cardUpdate, name="card-update"),
-    path('card-create/', views.cardCreate, name="card-Create"),
-    path('card-delete/<str:pk>/', views.cardDelete, name="card-Delete"),
+    path('', views.cardList, name="list"),
+    path('detail/<str:pk>/', views.cardDetail, name="detail"),
+    path('update/<str:pk>/', views.cardUpdate, name="update"),
+    path('create/', views.cardCreate, name="create"),
+    path('delete/<str:pk>/', views.cardDelete, name="delete"),
   ]
